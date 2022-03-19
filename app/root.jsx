132dfb284185708079e4082d585748aa703adbd6
Martín Ciparelli <mciparelli@gmail.com>
2022-03-19 18:17:48 -0300

    css hidden impl

--- app/root.jsx
+++ app/root.jsx
@@ -64,6 +64,11 @@ export default function App() {
           },
         },
       },
+      MuiHidden: {
+        defaultProps: {
+          implementation: 'css'
+        }
+      }
     },
   });
   return (
