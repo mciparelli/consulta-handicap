4ff293b70cda58c41eaa171dfa72f161815706ef
mciparelli <mciparelli@gmail.com>
2023-12-14 19:21:04 -0300

    aag api

--- app/api/aag.js
+++ app/api/aag.js
@@ -1,6 +1,6 @@
 async function getTarjetas(matricula) {
   const response = await fetch(
-    `https://www.aag.org.ar/cake/Usuarios/getTarjetas/${matricula}`,
+    `https://www.aag.org.ar/cake/Usuarios/getDiferencialesHandicap/${matricula}`,
   );
   const result = await response.json();
 
@@ -14,7 +14,7 @@ async function getTarjetas(matricula) {
     const PCC = tarjeta.PCC;
     const diferencial = tarjeta.Diferencial;
     const date = new Date(tarjeta.FechaTorneo);
-    const cargaDate = new Date(tarjeta.FechaCarga);
+    const cargaDate = new Date(tarjeta.FechaTorneo);
     // set to almost midnight to avoid having the date changed depending on where we are
     date.setHours(23);
     const id = `${clubId}-${date.getTime()}-${diferencial}`;
@@ -30,7 +30,7 @@ async function getTarjetas(matricula) {
       adjustedScore,
       courseRating,
       slopeRating,
-      is9Holes: tarjeta.TipoTarjeta === "9",
+      is9Holes: tarjeta.Atributo === "9",
       processed: tarjeta.Procesado,
     };
   });
